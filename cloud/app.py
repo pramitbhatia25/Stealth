47e29296d13cbc6d3e5d3e6ce8bb9b334e5fffaf
Pramit Bhatia <pramitbhatia25@gmail.com>
2025-02-21 11:45:35 -0500

    Edit

--- cloud/app.py
+++ cloud/app.py
@@ -10,12 +10,15 @@ import json
 from pydantic import BaseModel, ValidationError
 from typing import List
 from openai import OpenAI
+import requests
 
 openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 app = Flask(__name__)
 TABLE_ID = os.environ.get("TABLE_ID")
+TABLE_NEWS_ID = os.environ.get("TABLE_NEWS_ID")
 PROJECT_ID = os.environ.get("PROJECT_ID")
 SERVICE_ACCOUNT = os.environ.get("SERVICE_ACCOUNT")
+ALPHAVANTAGE_API_KEY=os.environ.get("ALPHAVANTAGE_API_KEY")
 app = Flask(__name__)
 CORS(app)
 
@@ -61,6 +64,76 @@ def fetch_current_crypto_data(tickers):
     df["Datetime"] = pd.to_datetime(df["Datetime"]).dt.strftime('%Y-%m-%d %H:%M:%S')
     
     return df
+
+
+def fetch_current_crypto_news(tickers):
+    all_data = []
+
+    for crypto in tickers:
+        try:
+            url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=CRYPTO:{crypto}&apikey={ALPHAVANTAGE_API_KEY}"
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an error for bad responses
+            data = response.json()
+            
+            if "feed" in data:
+                for article in data["feed"]:
+                    news_data = {
+                        "url": article.get("url", "No data"),
+                        "title": article.get("title", "No data"),
+                        "summary": article.get("summary", "No data"),
+                        "time_published": article.get("time_published", "No data"),
+                        "sentiment_score": article.get("overall_sentiment_score", "No data"),
+                        "sentiment_label": article.get("overall_sentiment_label", "No data"),
+                        "source": article.get("source", "No data"),
+                        "ticker_sentiment_score": article.get("ticker_sentiment", [{}])[0].get("ticker_sentiment_score", "No data"),
+                        "ticker_sentiment_label": article.get("ticker_sentiment", [{}])[0].get("ticker_sentiment_label", "No data")
+                    }
+                    all_data.append(news_data)
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data for {crypto}: {e}")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+        
+    df = pd.DataFrame(all_data)
+    print(df.head())
+    
+    # Explicitly convert the types to ensure compatibility with BigQuery
+    df["time_published"] = pd.to_datetime(df["time_published"]).dt.strftime('%Y-%m-%d %H:%M:%S')
+    df["sentiment_score"] = df["sentiment_score"].astype("float32")
+    df["ticker_sentiment_score"] = df["ticker_sentiment_score"].astype("float32")
+        
+    print(df.head())
+    return df
+
+
+def check_or_create_table_news(client, table_id, df):
+    try:
+        # Check if the table exists
+        client.get_table(table_id)
+        print(f"Table {table_id} already exists.")
+    except Exception:
+        print(f"Table {table_id} does not exist. Creating a new table.")
+
+        # Define schema based on the DataFrame columns
+        schema = []
+        for col in df.columns:
+            dtype = df[col].dtype
+
+            if dtype == "float32" or dtype == "float64":
+                schema.append(bigquery.SchemaField(col, "FLOAT"))
+            elif dtype == "datetime64[ns]":
+                schema.append(bigquery.SchemaField(col, "DATETIME"))
+            elif dtype == "object":  # Strings are usually detected as 'object'
+                schema.append(bigquery.SchemaField(col, "STRING"))
+            else:
+                # In case of other types, you can add more conditions as necessary
+                schema.append(bigquery.SchemaField(col, "STRING"))
+
+        # Create the table with schema
+        table = bigquery.Table(table_id, schema=schema)
+        client.create_table(table)
+        print(f"Table {table_id} created successfully.")
 
 
 def check_or_create_table(client, table_id, df):
@@ -126,6 +199,20 @@ def parse_and_upload_data_to_bq(df, TABLE_ID):
         print(f"Error while processing data for BigQuery upload: {e}")
 
 
+def get_existing_news_urls(table_id):
+    """Query BigQuery for existing news article URLs."""
+    credentials = service_account.Credentials.from_service_account_info(json.loads(SERVICE_ACCOUNT))
+    client = bigquery.Client(credentials=credentials, project=PROJECT_ID)
+    
+    query = f"SELECT DISTINCT url FROM `{table_id}`"
+    
+    query_job = client.query(query)
+    result = query_job.result()
+    
+    existing_urls = {row.url for row in result}
+    return existing_urls
+
+
 @app.route("/fetch-crypto-data", methods=["GET"])
 def get_crypto_data():
     crypto_symbols = [
@@ -135,6 +222,34 @@ def get_crypto_data():
     crypto_data = fetch_current_crypto_data(crypto_symbols)
     parse_and_upload_data_to_bq(crypto_data, TABLE_ID)
     return jsonify(crypto_data.to_dict(orient="records"))
+
+
+@app.route("/fetch-crypto-news", methods=["GET"])
+def get_crypto_news():
+    cryptos = ["BTC", "ETH", "BNB", "XRP", "ADA", "SOL", "DOGE", "DOT", "MATIC", "LTC", 
+               "BCH", "LINK", "XLM", "UNI", "ATOM", "ALGO", "VET", "ICP", "FIL", "MANA"]
+    
+    # Fetch new crypto news data
+    new_data = fetch_current_crypto_news(tickers=cryptos)
+    
+    # Get existing URLs from BigQuery
+    existing_urls = get_existing_news_urls(TABLE_NEWS_ID)
+
+    # Filter out articles with URLs that already exist
+    new_data_filtered = [entry for entry in new_data if entry["url"] not in existing_urls]
+    
+    # If there are new unique articles, upload to BigQuery
+    if new_data_filtered:
+        try:
+            df = pd.DataFrame(new_data_filtered)
+            parse_and_upload_data_to_bq(df, TABLE_NEWS_ID)            
+            print(f"Saved {len(new_data_filtered)} new articles.")
+        except Exception as e:
+            print(f"Error saving data to BigQuery: {e}")
+    else:
+        print("No new unique articles to save.")
+
+    return jsonify(df.to_dict(orient="records")) if not df.empty else jsonify([])
 
 
 def get_price_from_bq(symbol):
@@ -170,17 +285,71 @@ def get_price(symbol):
 
 
 @app.route("/get-all-prices", methods=["GET"])
-def get_all_prices():
+def get_latest_prices():
     credentials = service_account.Credentials.from_service_account_info(json.loads(SERVICE_ACCOUNT))
     client = bigquery.Client(credentials=credentials, project=PROJECT_ID)
+    
     query = f"""
-        SELECT Ticker, CurrentPrice FROM `{TABLE_ID}`
-        ORDER BY Datetime DESC;
+        SELECT Ticker, CurrentPrice, Datetime, Open, High, Low, Volume
+        FROM `{TABLE_ID}`
+        QUALIFY ROW_NUMBER() OVER (PARTITION BY Ticker ORDER BY Datetime DESC) = 1;
     """
+    
     query_job = client.query(query)
     result = query_job.result()
-    prices = {row.Ticker: row.CurrentPrice for row in result}
+    
+    # Create a list of dictionaries containing all the fields
+    prices = [
+        {
+            "Ticker": row.Ticker,
+            "CurrentPrice": row.CurrentPrice,
+            "Datetime": row.Datetime,
+            "Open": row.Open,
+            "High": row.High,
+            "Low": row.Low,
+            "Volume": row.Volume
+        }
+        for row in result
+    ]
+
+    print(jsonify(prices))
+    
     return jsonify(prices)
+
+
+@app.route("/get-news/<symbol>", methods=["GET"])
+def get_news(symbol):
+    # Load credentials and create BigQuery client
+    credentials = service_account.Credentials.from_service_account_info(json.loads(SERVICE_ACCOUNT))
+    client = bigquery.Client(credentials=credentials, project=PROJECT_ID)
+    
+    # Define the query using the symbol as the search query
+    query = f"""
+        SELECT 
+            url, 
+            title, 
+            time_published, 
+            sentiment_score, 
+            sentiment_label, 
+            source, 
+            ticker_sentiment_score, 
+            ticker_sentiment_label
+        FROM `{TABLE_NEWS_ID}`
+        WHERE 
+            LOWER(url) LIKE LOWER('%{symbol}%')
+            OR LOWER(title) LIKE LOWER('%{symbol}%')
+        LIMIT 50    
+    """
+    
+    # Execute the query and retrieve results
+    query_job = client.query(query)
+    result = query_job.result()
+    
+    # Convert the result into a list of dictionaries
+    news_list = [dict(row) for row in result]
+
+    # Return the results as JSON
+    return jsonify(news_list)
 
 
 @app.route("/get-price-history/<symbol>", methods=["GET"])
